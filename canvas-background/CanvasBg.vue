9208368a1a0b711e1c323aae463d81725963804b
LeoHsia <380015486@qq.com>
2020-11-29 01:40:37 +0800

    background with canvas
    由canvas实现的动画背景，几何图形为节点和连线，一定概率（可配置）出现带弧形的节点

---
+++ canvas-background/CanvasBg.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="canvas-bg">
+        <canvas id="canvas"></canvas>
+    </div>
+</template>
+
+<script>
+import CanvasBgWithEvent from './scripts/CanvasBgWithEvent'
+
+export default {
+    data() {
+        return {}
+    },
+    mounted() {
+        let inst = new CanvasBgWithEvent({
+            dom: document.querySelector('#canvas')
+        })
+    }
+}
+</script>
+
+<style scoped>
+.canvas-bg {
+    display: block;
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+    /* background: #aaa; */
+}
+
+#canvas {
+    width: 100%;
+    height: 100%;
+}
+</style>
